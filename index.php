556f033da279da3b041a68f7825935cb3bde1568
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-27 16:55:22 +0200

    add file

--- index.php
+++ index.php
@@ -3,8 +3,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+
+		<!-- UIkit CSS -->
+		<!-- https://getuikit.com/docs/installation -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
+
+	>
 </head>
 <body>
 	<h1>Hello world</h1>
+
+
+
+
+	<!-- ******************************************************************************** -->
+	<!-- jQuery is required -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script
 </body>
 </html>
