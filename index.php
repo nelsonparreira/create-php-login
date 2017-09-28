1a55009c5ff9af399a1ec821175cce2479530add
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-28 11:03:15 +0200

    Added login form

--- index.php
+++ index.php
@@ -8,12 +8,44 @@
 		<!-- https://getuikit.com/docs/installation -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
 
-	>
+	
 </head>
 <body>
-	<h1>Hello world</h1>
+	
+	<div class="uk-section uk-container ">
+		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required="required">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your password" required="required">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <div class="uk-form-controls">
+			            <button class="uk-button uk-button-default" type="submit">Login</button>
+			        </div>
+			    </div>
+
+					
 
 
+			    
+
+			</form>
+		</div>
+	</div>
+
+		
 
 
 	<!-- ******************************************************************************** -->
