195c193edca7289ca71e806b436259d7b5c78c93
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-28 17:12:00 +0200

    till min 3:26 of section 3, lesson 12

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,20 @@ $(document)
 
 	// this variable contains all the code inside <form>
 	var _form = $(this);
+	var _error = $(".js-error", _form);
 	//let's take the data that we wont from the _form, and put it on a obj
 	var data = {
 		email: $("input[type='email']", _form).val(),
 		password: $("input[type='password']", _form).val()
 	};
+
+	if (data.email.length < 6) {
+		_error
+				.text("Please enter a valid email address")
+				.show();
+		return false;
+	}
+
 
 	console.log(data);
 	// alert("YES Form was submited");
--- register.php
+++ register.php
@@ -38,10 +38,12 @@
 			        </div>
 			    </div>
 
-			    <div class="uk-margin">
-			        <div class="uk-form-controls">
-			            <button class="uk-button uk-button-default" type="submit">Register</button>
-			        </div>
+						<div class="uk-margin uk-alert uk-alert-danger js-error" style="display: none;">
+							test
+						</div>
+				
+			    <div class="uk-margin">  
+			            <button class="uk-button uk-button-default" type="submit">Register</button>  
 			    </div>
 
 					
