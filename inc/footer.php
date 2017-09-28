c2f44e442bc48851d7b2e0d7d07d927fc3dfa80f
nelsonparreira <nelsonblparreira@gmail.com>
2017-09-28 12:42:33 +0200

    Added config / login/register pages  / footer

---
+++ inc/footer.php
@@ -0,0 +1,15 @@
+<?php
+	//If there is no constant defined called __CONFIG__ do not load this file
+	if (!defined('__CONFIG__')) {
+		exit('You do not have a config file');
+		// normally send the user tto other page
+	}
+?>
+
+<!-- ******************************************************************************** -->
+	<!-- jQuery is required -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
+<?php
+	// Allow the config
+	define('__CONFIG__', true);
+	// Require the config
+	require_once 'inc/config.php';
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -13,47 +19,16 @@
 <body>
 	
 	<div class="uk-section uk-container ">
-		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
-
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required="required">
-			        </div>
-			    </div>
-
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your password" required="required">
-			        </div>
-			    </div>
-
-			    <div class="uk-margin">
-			        <div class="uk-form-controls">
-			            <button class="uk-button uk-button-default" type="submit">Login</button>
-			        </div>
-			    </div>
-
-					
-
-
-			    
-
-			</form>
-		</div>
+			<h2>Hello world. Today we are in <?= date("Y");?></h2>
+			<p>
+				<a href="login.php">Login</a>
+				<a href="register.php">Register</a>
+			</p>
+			
 	</div>
 
 		
 
-
-	<!-- ******************************************************************************** -->
-	<!-- jQuery is required -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-	<!-- UIkit JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script
+  <?php require_once 'inc/footer.php'; ?>
 </body>
 </html>
